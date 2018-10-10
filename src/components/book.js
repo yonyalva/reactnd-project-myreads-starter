352f794d2cd18a4e9a1c66ab28666135a099faaa
Alvarez <Yony.Alvarez@aig.com>
2018-10-10 17:52:18 -0400

    book change #2

--- src/components/book.js
+++ src/components/book.js
@@ -13,7 +13,7 @@ class book extends Component {
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${(this.props.book.imageLinks.thumbnail) ? this.props.book.imageLinks.thumbnail : ""})`}}></div>
                             <div className="book-shelf-changer">
-                              <select>
+                              <select value={this.props.book.shelf || "None"}>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
